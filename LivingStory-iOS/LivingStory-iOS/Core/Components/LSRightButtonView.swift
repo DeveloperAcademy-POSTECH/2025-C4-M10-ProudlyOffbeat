354904f5eb2741201987e7ae7ba439d3948cac7e
Ito <ckdwo23@gmail.com>
2025-07-22 12:36:02 +0900

    feat: Componets 수정 -> 버튼 두 개 각각 컴포넌트화

---
+++ LivingStory-iOS/LivingStory-iOS/Core/Components/LSRightButtonView.swift
@@ -0,0 +1,22 @@
+//
+//  LSRightButtonView.swift
+//  LivingStory-iOS
+//
+//  Created by 문창재 on 7/22/25.
+//
+import SwiftUI
+
+struct LSRightButtonView: View {
+    let action: () -> Void
+    var body: some View {
+        HStack{
+            Spacer()
+            Button{
+                action()
+            }label: {
+                Image("NextButton")
+                    .padding()
+            }
+        }
+    }
+}
