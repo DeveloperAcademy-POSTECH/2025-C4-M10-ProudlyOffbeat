27ca42a1d71be7a3e82a0f3d8a33c39fbbd85cf7
G.M_YOO <ykm7003@gmail.com>
2025-07-28 03:10:42 +0900

    chore: soundResources 추가
    Refs: #71

---
+++ LivingStory-iOS/LivingStory-iOS/Core/Literals/SoundLiterals.swift
@@ -0,0 +1,13 @@
+//
+//  SoundLiterals.swift
+//  LivingStory-iOS
+//
+//  Created by Demian Yoo on 7/28/25.
+//
+
+import Foundation
+
+enum SoundLiterals {
+    static let pigBackgroundMusic = "pigBackgroundMusic"
+    static let pigBlowSound = "pigBlowEffect"
+}
